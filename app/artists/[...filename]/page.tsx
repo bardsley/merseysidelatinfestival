dfda64227ad0f473be834facb088cb28da40f443
Adam bardsley <adam.bardsley@onevoicedigital.com>
2024-07-15 01:50:56 +0100

    Comment Unused

--- app/artists/[...filename]/page.tsx
+++ app/artists/[...filename]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import client from "../../../tina/__generated__/client";
 import Layout from "../../../components/layout/layout";
-import ArtistClientPage from "./client-page";
+// import ArtistClientPage from "./client-page";
 
 export default async function PostPage({
   params,
