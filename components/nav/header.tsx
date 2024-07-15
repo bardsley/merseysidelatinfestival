c5257334b3c22ac7555ba964e2d5cf0e21b820e8
Adam bardsley <adam.bardsley@onevoicedigital.com>
2024-07-15 18:01:18 +0100

    Missed useState

--- components/nav/header.tsx
+++ components/nav/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { useState } from 'react'
 import Link from "next/link";
 import { Container } from "../layout/container";
 import { cn } from "../../lib/utils";
