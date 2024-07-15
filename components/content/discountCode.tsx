48cef8fb6df47d298cb5b527dfffd1110c0022ba
Adam bardsley <adam.bardsley@onevoicedigital.com>
2024-07-15 18:00:25 +0100

    Remove uneeded for TS

--- components/content/discountCode.tsx
+++ components/content/discountCode.tsx
@@ -1,5 +1,3 @@
-import { TinaMarkdown, TinaMarkdownContent } from "tinacms/dist/rich-text";
-
 const DiscountCode = (props: {code: string }) => {
   return (
     <div className="block text-2xl text-gold-400 border-gold-700 border-3 rounded-lg border-dashed py-3 px-6 mx-auto md:mx-0 w-fit">      
