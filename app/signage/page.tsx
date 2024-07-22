05f0b259f6025509947ce3b03bb85075990e64a6
Adam Bardsley <adam.bardsley@onevoicedigital.com>
2024-07-22 20:41:35 +0100

    basic page for signage

---
+++ app/signage/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default async function SignagePage() {
+  return (
+    <div className="mb-3 mx-auto my-12">
+      <h1 className="text-5xl font-bold">Example</h1>
+    </div>
+  )
+}
