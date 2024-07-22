c5813f8e20e4a079def0a35149fdd29d934b4853
Adam bardsley <adam.bardsley@onevoicedigital.com>
2024-07-22 20:54:07 +0100

    Signage Page update

--- app/signage/page.tsx
+++ app/signage/page.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import client from "../../tina/__generated__/client";
+import Layout from "../../components/layout/layout";
 
-export default async function SignagePage() {
+export default async function SignagePage({
+  params,
+}: {
+  params: { filename: string[] };
+}) {
+  const data = await client.queries.page({
+    relativePath: `home.mdx`,
+  });
+
   return (
-    <div className="mb-3 mx-auto my-12">
-      <h1 className="text-5xl font-bold">Example</h1>
-    </div>
-  )
+    <Layout rawPageData={data}>
+      <h1 className="text-5xl">Hello</h1>
+    </Layout>
+  );
+}
+
+export async function generateStaticParams() {
+  const pages = await client.queries.pageConnection();
+  const paths = pages.data?.pageConnection.edges.map((edge) => ({
+    filename: edge.node._sys.breadcrumbs,
+  }));
+  return paths || [];
 }
