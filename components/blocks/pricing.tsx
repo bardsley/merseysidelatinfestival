82d3c6e8c7fc418d131731f5113383a3f8607a7d
bardsley <adam.bardsley@gmail.com>
2024-07-29 18:16:48 +0100

    Checkout finessing -  ScrollBetter - commit Darft mode greys to make clear waht's live and is always shown in dev - Removing old files

--- components/blocks/pricing.tsx
+++ components/blocks/pricing.tsx
@@ -18,8 +18,9 @@ export const Pricing = ({ data }: { data: PageBlocksPricing }) => {
     if (packagesSuggestorRef.current) {
       setTimeout(() => {
         const bottom = packagesSuggestorRef.current.getBoundingClientRect().bottom
+        console.log(packagesSuggestorRef.current.getBoundingClientRect())
         window && window.scrollBy({
-          top: bottom + 400,
+          top: bottom -699 ,
           behavior : "smooth"
         })
       },100)
@@ -127,10 +128,11 @@ export const Pricing = ({ data }: { data: PageBlocksPricing }) => {
     </Section>
     <Section>
       <Container size='medium' width="huge">
-    <PricingTable fullPassFunction={setFullPassSelectFunction} scrollToElement={scrollToElement}></PricingTable>
+        <div ref={packagesSuggestorRef}>
+          <PricingTable fullPassFunction={setFullPassSelectFunction} scrollToElement={scrollToElement}></PricingTable>
+        </div>
       </Container>
     </Section>
-    <a className="block border border-gold-700 scroll-mb-40" ref={packagesSuggestorRef} id="package"></a>
     </>
     
   );
