eb098736b7f2a8d4ffab062034e96874233268fd
bardsley <adam.bardsley@gmail.com>
2024-07-30 00:35:04 +0100

    TS fixes

--- components/admin/ticketList.tsx
+++ components/admin/ticketList.tsx
@@ -1,7 +1,6 @@
 import {format } from 'date-fns'
-import { use } from 'react';
 
-const shuffle = (array: object[]) => { 
+const shuffle = (array: any[]) => { 
   for (let i = array.length - 1; i > 0; i--) { 
     const j = Math.floor(Math.random() * (i + 1)); 
     [array[i], array[j]] = [array[j], array[i]]; 
