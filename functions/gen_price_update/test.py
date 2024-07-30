f1c62bf2df95c7bcc83523a61741d1698a4f6e52
Connor Monaghan <connor1monaghan@gmail.com>
2024-07-30 22:03:36 +0200

    Update test.py

--- functions/gen_price_update/test.py
+++ functions/gen_price_update/test.py
@@ -8,19 +8,3 @@ boto3.setup_default_session(profile_name=profile_name)
 logging.basicConfig()
 
 products = lambda_handler(None, None)
-
-# auth = Auth.Token(os.environ.get("GITHUB_TOKEN"))
-# git = Github(auth=auth)
-# # git.get_user().login
-
-# repo = git.get_repo("bardsley/merseysidelatinfestival")
-
-# path = "components/ticketing/pricingDefaultsDynamic.ts"
-# branch = "pricing"
-
-# try:
-#     contents = repo.get_contents(path, branch)
-# except:
-#     repo.create_file(path, "create dynamic pricing file", content, branch=branch, author=author)
-#     print("File not found")
-# pprint(contents.decoded_content.decode("utf-8"), width=200)
