d369b0d5271b61e62e8cb939e9dfd2004f455d2b
Connor Monaghan <connor1monaghan@gmail.com>
2024-08-01 22:55:45 +0200

    transfer ticket to new email

--- functions/transfer_owner/lambda_function.py
+++ functions/transfer_owner/lambda_function.py
@@ -12,6 +12,7 @@ import time
 import json
 import logging
 from json.decoder import JSONDecodeError
+from decimal import Decimal
 
 import boto3
 from boto3.dynamodb.conditions import Key
@@ -71,6 +72,11 @@ def update_table(input, key, condition=None):
         logger.error("The ticket does not exist")
         raise ValueError("Ticket number does not exist or match email.")
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return super().default(obj)
 
 def lambda_handler(event, context):
     try:
@@ -80,8 +86,8 @@ def lambda_handler(event, context):
         logger.error(e)
         return err("An error has occured with the input you have provied.", event_body=event['body'])
     
-    if ('ticket_number' not in data) and ('email' not in data) and ('email_to' not in data) and ('name_to' not in data):
-        return err("Must provide ticket_number, email, email_to, name_to.")
+    if ('ticket_number' not in data) and ('email' not in data) and ('email_to' not in data) and ('name_to' not in data) and ('phone_to' not in data):
+        return err("Must provide ticket_number, email, email_to, name_to, phone_to.")
     else:
         ticket_number = int(data['ticket_number'])
         email = data['email']
@@ -102,30 +108,98 @@ def lambda_handler(event, context):
             'full_name': data['name_to'],
             'transferred': {
                 'date': int(time.time()),
-                'ticket_number': data['ticket_number'],
-                'email': data['email'],
+                'ticket_number': ticket_number,
+                'email': ticket_entry['email'],
                 'full_name': ticket_entry['full_name'],
                 'source': data['source'] #! check this here rather than on client
             }
         }
+        update_table(input, {'email': data['email'], 'ticket_number':data['ticket_number']})
 
         logger.info("Invoking send_email lambda")
         response = lambda_client.invoke(
             FunctionName='dev-send_email',
             InvocationType='Event',
             Payload=json.dumps({
+                    'email_type':"transfer_ticket",
                     'name':data['name_to'], 
-                    'email':email, 
+                    'email':data['email_to'], 
                     'ticket_number':ticket_number, 
                     'line_items':ticket_entry['line_items'], 
+                    'email_from': ticket_entry['email'],
+                    'full_name_from': ticket_entry['full_name'],
                     'heading_message': "A TICKET HAS BEEN TRANSFERRED TO YOU!"
                 }),
             )
         logger.info(response)
         
-        update_table(input, {'email': data['email'], 'ticket_number':data['ticket_number']})
-        return {'statusCode': 200, 'body': json.dumps({'message':"Still working on it (Connor)"})}
+        return {'statusCode': 200, 'body': json.dumps({'message':"Ticket transferred"})}
     else:
-        #TODO
-        return
+        #   create a new ticket with a new ticket number
+        #   make the old ticket inactive and state that it has been transferred and to who
+        #   send the new ticket to the new owner and tell them who it came from
+        #   send email to old owner telling them they transferred it and who to
+        input = {
+            'full_name': data['name_to'],
+            'active': False,
+            'transferred': {
+                'date': int(time.time()),
+                'ticket_number': ticket_number,
+                'email': ticket_entry['email'],
+                'full_name': ticket_entry['full_name'],
+                'source': data['source'] #! check this here rather than on client
+            }
+        }
+        update_table(input, {'email': data['email'], 'ticket_number':ticket_number})
+
+        previous_owner = [json.dumps({
+            'date': int(time.time()),
+            'ticket_number': ticket_number,
+            'email': data['email'],
+            'full_name': ticket_entry['full_name'],
+            'source': data['source'] #! check this here rather than on client
+        })]
+
+
+        # Create a new ticket
+        logger.info("Invoking create_ticket lambda")
+        create_ticket = lambda_client.invoke(
+            FunctionName='dev-create_ticket',
+            InvocationType='Event',
+            Payload=json.dumps({
+                'email': data['email_to'],      
+                'full_name': data['name_to'],
+                'phone': data['phone_to'],
+                'purchase_date': ticket_entry['purchase_date'],
+                'line_items': ticket_entry['line_items'],
+                'access': ticket_entry['access'],
+                'status': ticket_entry['status'],
+                'student_ticket': ticket_entry['student_ticket'],
+                'promo_code': ticket_entry['promo_code'],
+                'meal_preferences': ticket_entry['meal_preferences'],
+                'checkout_session': ticket_entry['checkout_session'] if 'checkout_session' in ticket_entry else None, 
+                'schedule': ticket_entry['schedule'],
+                'owner_history': previous_owner+ticket_entry['owner_history']
+                },cls=DecimalEncoder),
+            )
+        logger.info(create_ticket)      
+
+        logger.info("Invoking send_email lambda")
+        response = lambda_client.invoke(
+            FunctionName='dev-send_email',
+            InvocationType='Event',
+            Payload=json.dumps({
+                    'email_type':"transfer_ticket",
+                    'name':data['name_to'], 
+                    'email':data['email_to'], 
+                    'ticket_number':create_ticket['ticket_number'], 
+                    'line_items':ticket_entry['line_items'], 
+                    'email_from': ticket_entry['email'],
+                    'full_name_from': ticket_entry['full_name'],
+                    'heading_message': "A TICKET HAS BEEN TRANSFERRED TO YOU!"
+                }),
+            )
+        logger.info(response)
+        
+        return {'statusCode': 200, 'body': json.dumps({'message':"Ticket transferred"})}
 
