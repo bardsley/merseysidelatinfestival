5335990c62eb2745f450a27b537c5cf5807dffdf
bardsley <adam.bardsley@gmail.com>
2024-08-06 21:11:30 +0100

    Ticket page now shows basic info

---
+++ app/admin/ticketing/ticket/[ticket_number]/[email]/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Layout from "../../../../../../components/layout/layout";
+import { Container } from "../../../../../../components/layout/container";
+import Navigation from "../../../../../../components/admin/navigation";
+import TicketView from "../../../../../../components/admin/ticket";
+export default async function AdminTicketPage({ params }: { params: { ticket_number: string, email: string } }) {
+
+  const pages = [
+    { name: 'Admin', href: '/admin', current: true },
+    { name: 'Ticketing', href: '/admin/ticketing', current: true },
+    { name: 'View Ticket', href: `/admin/ticketing/${params.ticket_number}/${params.email}`, current: true },
+  ]
+
+  return (<Layout>
+    <section className={`flex-1 relative transition duration-150 ease-out body-font overflow-hidden bg-none text-white`}>
+      {" "}
+      <Container width="large" padding="tight" className={`flex-1 pb-2`} size="top">
+        <Navigation pages={pages} />
+      </Container>             
+      
+      <Container width="small" padding="tight" className={`flex-1 pb-2`} size="none">
+        <TicketView ticket_number={params.ticket_number} email={params.email}  ></TicketView>
+      </Container>
+    </section>
+  </Layout>
+);
+}
