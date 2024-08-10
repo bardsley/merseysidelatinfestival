917b9b4bed8d71e498ac2310993966d39983ef59
bardsley <adam.bardsley@gmail.com>
2024-08-10 20:10:47 +0100

    Adding requester info for admin calls (#96)

--- components/admin/modals/nameChangeModal.tsx
+++ components/admin/modals/nameChangeModal.tsx
@@ -68,7 +68,7 @@ export default function NameChangeModal({ticket,open,onClose,refreshFunction}) {
               <button
                 type="submit"
                 disabled={isSubmitting}
-                className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
               >
                 Change
               </button>
