cded9e9b3094bc332d2aa865b4bbdb49025f9563
bardsley <adam.bardsley@gmail.com>
2024-08-13 16:23:46 +0100

    Changing test to not include text present in Instagram

--- tests/website/navigation.spec.ts
+++ tests/website/navigation.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, type Page } from '@playwright/test';
 
 test('Homepage', async ({ page }: {page: Page}) => {
   await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'Merseyside Latin Festival' })).toHaveCount(2);
+  await expect(page.getByRole('heading', { name: 'Biggest & Best SRBK Event' })).toBeVisible();
   await page.goto('/meal');
   await expect(page.getByRole('heading', { name: 'Dinner' })).toBeVisible();
   await page.goto('/tickets');
