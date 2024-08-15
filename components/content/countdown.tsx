b5097617e9640ebf380a3d840fac79a695ed31c7
bardsley <adam.bardsley@gmail.com>
2024-08-16 00:03:49 +0100

    Countdown has configurable duration and messages

--- components/content/countdown.tsx
+++ components/content/countdown.tsx
@@ -5,23 +5,29 @@ const valueClasses = " text-2xl md:text-5xl font-bold";
 const containerClasses = "flex flex-wrap max-w-md gap-x-6 justify-center items-center border border-richblack-700  bg-richblack-500 p-6 rounded-xl mb-3";
 const headerClasses = "w-full text-center text-lg"
 
-const preEventRenderer = ({ days, hours, minutes, seconds, completed }) => {
- 
+const preEventRenderer = ({ days, hours, minutes, seconds, completed, props }) => {
+  const message = {
+    before: props['data-message-before'] ? props['data-message-before'] : "That's right, only {days} sleeps to go!", 
+    today: props['data-message-today'] ? props['data-message-today'] : "It's today!",    
+    during: props['data-message-during'] ? props['data-message-during'] : "It's happening right now!",    
+    after: props['data-message-after'] ? props['data-message-after'] : "It's already started!",
+  }
   // const eventLength = {days: 2, hours: 6, minutes: 60, seconds: 60};
   if (completed) {
     // Render a completed state
     return <div className={containerClasses}>
-            <h2 className="w-full text-center text-3xl font-bold">It&apos;s already started!</h2>
+            <h2 className="w-full text-center text-3xl font-bold">{ message.after}</h2>
             </div>
   } else {
     // Render a countdown
     return (<div className={containerClasses}>
       { days > 0 
-        ? <div className={headerClasses}>That&apos;s right, only {days} sleeps to go!</div> 
-        : <div className="w-full text-center text-3xl font-bold">It&apos;s today!</div> }
+        ? <div className={headerClasses}>{message.before.replace('{days}', `${days}`)}</div> 
+        : <div className="w-full text-center text-3xl font-bold">{message.today}</div> 
+     }
 
       { days > 0 ? <div><span className={valueClasses}>{days}</span> days</div> : null }
-      { hours > 0 ? <div><span className={valueClasses}>{hours}</span> hours</div> :null }
+      { hours > 0  && days > 0 ? <div><span className={valueClasses}>{hours}</span> hours</div> :null }
       { minutes > 0 ? <div><span className={valueClasses}>{minutes}</span> mins</div> : null }
       { days < 1 ?  <div><span className={valueClasses}>{seconds}</span> secs </div> : null }
       </div>
@@ -29,26 +35,31 @@ const preEventRenderer = ({ days, hours, minutes, seconds, completed }) => {
   }
 };
 
-const duringEventRenderer = ({ total, days, hours, minutes, seconds, completed }) => {
-  
+const duringEventRenderer = ({ days, hours, minutes, seconds, completed, props}) => {
+  const message = {
+    before: props['data-message-before'] ? props['data-message-before'] : "That's right, only {days} sleeps to go!", 
+    today: props['data-message-today'] ? props['data-message-today'] : "It's today!",    
+    during: props['data-message-during'] ? props['data-message-during'] : "It's happening right now!",    
+    after: props['data-message-after'] ? props['data-message-after'] : "It's already started!",
+  }
 
   // const eventLength = {days: 2, hours: 6, minutes: 60, seconds: 60};
   if (completed) {
     // Render a completed state
     return <div className={containerClasses}>
-            <h2 className="w-full text-center text-3xl font-bold">It&apos;s already started?</h2>
-            { 2-days > 0 ? <div><span className={valueClasses}>{2-days}</span> days</div> : null }
-            { 6 - hours > 0 ? <div><span className={valueClasses}>{6-hours}</span> hours</div> :null }
-            { 60 - minutes > 0 ? <div><span className={valueClasses}>{60 -minutes}</span> mins</div> : null }
-            { days > 2 ?  <div><span className={valueClasses}>{60 - seconds}</span> secs </div> : null }
-            <h2 className="w-full text-center text-3xl font-bold">left, Get down here!</h2>
-            <div className="w-full text-center text-3xl font-bold">{days}:{hours}:{minutes}:{seconds} = {total/1000}</div>
+            <div className="w-full text-center text-lg ">{message.after}</div> 
+            { days > 0 ? <div><span className={valueClasses}>{days}</span> days</div> : null }
+            { hours > 0 ? <div><span className={valueClasses}>{hours}</span> hours</div> :null }
+            { minutes > 0 ? <div><span className={valueClasses}>{minutes}</span> mins</div> : null }
+            { days < 1 ?  <div><span className={valueClasses}>{seconds}</span> secs </div> : null }
+            {/* <h2 className="w-full text-center text-3xl font-bold"></h2> */}
+            {/* <div className="w-full text-center text-3xl font-bold">{days}:{hours}:{minutes}:{seconds} = {total/1000}</div> */}
             </div>
   } else {
     // Render a countdown
     return (<div className={containerClasses}>
   
-      <div className="w-full text-center text-lg ">It&apos; Started! We dance for another</div> 
+      <div className="w-full text-center text-lg ">{message.during}</div> 
       { days > 0 ? <div><span className={valueClasses}>{days}</span> days</div> : null }
       { hours > 0 ? <div><span className={valueClasses}>{hours}</span> hours</div> :null }
       { minutes > 0 ? <div><span className={valueClasses}>{minutes}</span> mins</div> : null }
@@ -58,7 +69,7 @@ const duringEventRenderer = ({ total, days, hours, minutes, seconds, completed }
   }
 };
 
-export const CountdownElement = (props: {date: string}) => {
+export const CountdownElement = (props: {date: string, message_before: string, message_today: string, message_after: string, message_during:string, duration:number}) => {
   const [ isLoaded, setIsLoaded] = useState(false)
 
   useEffect(() => {
@@ -66,22 +77,57 @@ export const CountdownElement = (props: {date: string}) => {
   }, [])
   const dateToCountTo = new Date(props.date)
   const inTheFuture = Date.now() - dateToCountTo.getTime() < 0
-  const endOfEvent = new Date(dateToCountTo.getTime() + 56*60*60*1000) 
-  const countDownTimer = inTheFuture ? <Countdown overtime={true} date={dateToCountTo} renderer={preEventRenderer}/> : <Countdown overtime={true} date={endOfEvent} renderer={duringEventRenderer}/>
+  // const endOfEvent = new Date(dateToCountTo.getTime() + 56*60*60*1000) 
+  const eventDuration = props.duration ? props.duration * 60 *1000 : 56*60*60*1000
+  const endOfEvent = new Date(dateToCountTo.getTime() + eventDuration) 
+  const countDownTimer = inTheFuture 
+    ? <Countdown overtime={true} date={dateToCountTo} renderer={preEventRenderer} 
+      data-message-before={props.message_before}
+      data-message-during={props.message_during}
+      data-message-today={props.message_today}
+      data-message-after={props.message_after}/> 
+    : <Countdown overtime={true} date={endOfEvent} renderer={duringEventRenderer}
+      data-message-during={props.message_during}
+      data-message-after={props.message_after}/>
   return isLoaded && countDownTimer
 }
 
 export const CountdownElementTemplate = {
-    name: "CountdownElement",
-    label: "Countdown Element",
-    fields: [{
-      name: "date",
-      label: "Date",
-      type: "datetime",
-      ui: {
-        timeFormat: "HH:mm"
-      },
-    }]
+  name: "CountdownElement",
+  label: "Countdown Element",
+  fields: [{
+    name: "date",
+    label: "Date",
+    type: "datetime",
+    ui: {
+      timeFormat: "HH:mm"
+    },
+  }, {
+    name: "message_before",
+    label: "Message Before",
+    type: "string",
+  },
+  {
+    name: "message_today",
+    label: "Message on the day",
+    type: "string",
+  }, 
+  {
+    name: "message_during",
+    label: "Message during",
+    type: "string",
+  }, 
+  {
+    name: "duration",
+    label: "Duration of thing in minutes",
+    type: "number",
+  }, 
+  {
+    name: "message_after",
+    label: "Message After",
+    type: "string",
+  }, 
+  ]
 }
 
 export default CountdownElement
