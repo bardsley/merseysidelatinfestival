a73aa326c0723effb066926e3e192db2a62c9f1f
Connor Monaghan <connor1monaghan@gmail.com>
2024-10-11 17:04:08 +0200

    table group lambda

--- functions/table_group/lambda_function.py
+++ functions/table_group/lambda_function.py
@@ -3,7 +3,9 @@ from boto3.dynamodb.conditions import Key, Attr
 import json
 import logging
 from shared import DecimalEncoder as shared
+from json.decoder import JSONDecodeError
 import os
+import time
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
@@ -21,65 +23,106 @@ def err(msg:str, code=400, logmsg=None, **kwargs):
         'body': json.dumps({'error': msg})
         }
 
-def get_table_group(group_id):
-    '''
-
-    '''
-    response = table.query(KeyConditionExpression=Key('PK').eq("TABLEGROUP#{}".format(group_id)))
-    if response['Count'] == 0: 
-        logger.info("Table group does not exist")
-        return None
-    else:
-        return response['Count'], response['Items'][0]
-
-def post(event):
-    return
-
 def get(event):
     # Lookup table group name to check it
     data = event['queryStringParameters']
     if ('group_id' not in data):
-        logger.error("group_id nott set")
+        logger.error("group_id not set")
         return err("Must provide group_id.")
     else:
         group_id = data['group_id']
 
     logger.info("Looking up table group {}".format(group_id))
-    try:
-        count, group_object = get_table_group(group_id)
-    except ValueError as e:
-        err(str(e))
-    
-    if group_object == None:
-        return     {'statusCode': 200, 'body': {'code': 1, 'text': "Group does not exist."}}
+    response = table.query(KeyConditionExpression=Key('PK').eq("GROUP#{}".format(group_id)))
+
+    if response['Count'] == 0:
+        return {'statusCode': 200, 'body': {'code': 1, 'text': "Group does not exist."}}
     else:
-        if count >= group_object['capacity']:
-            return {'statusCode': 200, 'body': {'code': 2, 'text': "Group is full."}}
-        elif count < group_object['capacity']:
-            if data['group_id'] == True:
-                add()
-                return {'statusCode': 200, 'body': {'code': 3, 'text': "Group exists and is not full. Users place reserved in group."}}
-            elif data['group_id'] == False:
-                return {'statusCode': 200, 'body': {'code': 4, 'text': "Group exists and is not full."}}
+        return {'statusCode': 200, 'body': {'code': 2, 'text': "Group exists."}}
+
+def post(event):
+    # create new entry
+    try:
+        data = json.loads(event['body'])
+        logger.info(data)
+    except (TypeError, JSONDecodeError) as e:
+        logger.error(e)
+        return err("An error has occured with the input you have provied.", event_body=event['body'])
+    
+    if ('ticket_number' not in data) and ('email' not in data) and ('group_id' not in data) and ('timestamp' not in data):
+        return err("Must provide ticket_number, email, timestamp, and group_id.")    
+
+    _post(data['group_id'], data['ticket_number'], data['email'], data['timestamp'])
+
+    return True
+
+def _post(group_id, ticket_number, email, timestamp):
+    table.put_item(Item={
+        'PK': "GROUP#{}".format(group_id),
+        'SK': "ATTENDEE#{}".format(ticket_number),
+        'email': email,
+        'timestamp': timestamp
+    })
+
+    return True
+
+def delete(event):
+    # delete entry
+    try:
+        data = json.loads(event['body'])
+        logger.info(data)
+    except (TypeError, JSONDecodeError) as e:
+        logger.error(e)
+        return err("An error has occured with the input you have provied.", event_body=event['body'])
+
+    if ('ticket_number' not in data) and ('email' not in data) and ('group_id' not in data):
+        return err("Must provide ticket_number, email, and group_id.")    
+    
+    _delete(data['group_id'], data['ticket_number'])
+
+    return True
+
+def _delete(group_id, ticket_number):    
+    table.delete_item(
+        Key={
+            'PK': "GROUP#{}".format(group_id),
+            'SK': "ATTENDEE#{}".format(ticket_number)           
+        }
+    )
+    return True
 
 def patch(event):
-    # update an already entered table group entry
-    return
+    # update an entry i.e. delete old one with old details and make a new one
+    try:
+        data = json.loads(event['body'])
+        logger.info(data)
+    except (TypeError, JSONDecodeError) as e:
+        logger.error(e)
+        return err("An error has occured with the input you have provied.", event_body=event['body'])
+    
+    _delete(data['old']['group_id'], data['old']['ticket_number'])
+
+    _post(data['new']['group_id'], data['new']['ticket_number'], data['new']['email'], time.time())
+
+    return True
 
 def lambda_handler(event, context):
     '''
 
     '''    
     logger.info('## NEW REQUEST')
-    if event['requestContext']['http']['method'] == 'POST':
-        logger.info('## POST')
-        return post(event)
-    elif event['requestContext']['http']['method'] == 'GET':
+    if event['requestContext']['http']['method'] == 'GET':
         logger.info('## GET')
         return get(event)
+    elif event['requestContext']['http']['method'] == 'POST':
+        logger.info('## POST')
+        return post(event)
+    elif event['requestContext']['http']['method'] == 'DELETE':
+        logger.info('## DELETE')
+        return delete(event)   
     elif event['requestContext']['http']['method'] == 'PATCH':
         logger.info('## PATCH')
-        return patch(event)            
+        return patch(event)                    
     else:
         logger.info('## Erm....')
         return {
