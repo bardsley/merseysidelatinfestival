d6c7046ee1c24c24a3329446e4430a88b7a126b1
Connor Monaghan <connor1monaghan@gmail.com>
2024-10-29 21:45:35 +0100

    dump meal group data

--- functions/meal_groups/lambda_function.py
+++ functions/meal_groups/lambda_function.py
@@ -1,11 +1,36 @@
 import json
+import os
+import logging
+
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from shared import DecimalEncoder
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 ## ENV
-attendees_table_name = os.environ.get("ATTENDEES_TABLE_NAME")
+event_table_name = os.environ.get("EVENT_TABLE_NAME")
+logger.info(event_table_name)
+
+dynamodb_client = boto3.client('dynamodb')
+db = boto3.resource('dynamodb')
+table = db.Table(event_table_name)
+
+def err(msg:str, code=400, logmsg=None, **kwargs):
+    logmsg = logmsg if logmsg else msg
+    logger.error(logmsg)
+    for k in kwargs:
+        logger.error(k+":"+kwargs[k])
+    return {
+        'statusCode': code, 
+        'body': json.dumps({'error': msg})
+        }
 
 def lambda_handler(event, context):
-    # TODO implement
+    scan_response = table.scan(FilterExpression=Key('PK').begins_with('GROUP#'))
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(scan_response, cls=DecimalEncoder.DecimalEncoder)
     }
