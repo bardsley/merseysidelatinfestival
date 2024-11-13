c0467c7b9d108c4f222b8e05306a1414ac8402ec
Adam Bardsley <adam.bardsley@gmail.com>
2024-11-13 21:48:49 +0000

    Add Reception as a location

--- tina/collection/options.ts
+++ tina/collection/options.ts
@@ -4,5 +4,6 @@ export const locations = {
   sefton: { title: "Sefton", description: "",} ,
   hypostyle: { title: "Hypostyle", description: "",} ,
   terrace: { title: "Terrace", description: "",} ,
+  all: { title: "Reception", description: "" }
 };
 export const locationOptions = Object.keys(locations).map((loc) => ({label: locations[loc].title, value: loc}));
