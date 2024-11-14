9c9b8c8870b2aebb5785ba6d9cfe4dbebb992df3
Adam Bardsley <adam.bardsley@gmail.com>
2024-11-14 03:35:25 +0000

    Color fix final

--- tina/collection/sessionLevels.ts
+++ tina/collection/sessionLevels.ts
@@ -1,7 +1,7 @@
 export const levels = {
   advanced: { title: "Advanced", colour: "#ff5757",} ,
   intermediate: { title: "Intermediate", colour: "#ff914d",} ,
-  improvers: { title: "Improvers", colour: "#ffde59",} ,
+  improver: { title: "Improver", colour: "#ffde59",} ,
   beginner: { title: "Beginner", colour: "#7ed957",} ,
   all: { title: "Suitable for All", colour: "#0cc0df",} ,
 };
