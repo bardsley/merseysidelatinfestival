c4062e1157f39c5018908ca16a7596e116fd0614
Adam Bardsley <adam.bardsley@gmail.com>
2024-11-14 03:46:02 +0000

    Additional colours

--- tina/collection/sessionLevels.ts
+++ tina/collection/sessionLevels.ts
@@ -4,5 +4,8 @@ export const levels = {
   improver: { title: "Improver", colour: "#ffde59",} ,
   beginner: { title: "Beginner", colour: "#7ed957",} ,
   all: { title: "Suitable for All", colour: "#0cc0df",} ,
+  party: { title: "Party & Celebrations", colour: '#d0e5f1' },
+  shows: { title: "Shows", colour: '#ff66c4' },
+  admin: { title: "Registration & Admin", colour: '#2f454b'}
 };
 export const levelOptions = Object.keys(levels).map((lvl) => ({label: levels[lvl].title, value: lvl}));
