19f07c5f4d5c904ceb81fd93d62fa628c9cbaf50
Connor Monaghan <connor1monaghan@gmail.com>
2024-11-17 04:00:51 +0100

    add icons to sorter

--- components/mealTableSorter.tsx
+++ components/mealTableSorter.tsx
@@ -2,15 +2,17 @@
 import React, { useState } from "react";
 import useSWR from 'swr';
 import Link from 'next/link'
-import { fetcher } from "@lib/fetchers"; // Adjust the import path if necessary
-import { Bars3Icon, ArrowUturnLeftIcon, LockOpenIcon, LockClosedIcon, ArrowRightIcon} from '@heroicons/react/24/solid'; // Importing icons
+import { fetcher } from "@lib/fetchers"; 
+import { Bars3Icon, ArrowUturnLeftIcon, LockOpenIcon, LockClosedIcon} from '@heroicons/react/24/solid';
+import { BiLogoSketch } from 'react-icons/bi';
+import { RiVipFill } from "react-icons/ri";
+import { MdRestaurantMenu } from "react-icons/md";
 
 const mealTableApiUrl = "/api/admin/meal/seating"; // Update with the actual API URL
 const submitApiUrl = "/api/admin/meal/seating"; // Update with the actual submission API URL
 const maxTableCapacity = 12; // Set maximum allowed capacity to 12
 
 export default function MealTableSorter() {
-  const [isCollapsed, setIsCollapsed] = useState(true);
   const [fixedSeating, setFixedSeating] = useState<{ [key: string]: number }>({});
   const [movedRows, setMovedRows] = useState<{ [key: string]: number }>({});
   const [originalTableData, setOriginalTableData] = useState<any>(null);
@@ -263,6 +265,15 @@ export default function MealTableSorter() {
                                 <td className={`py-1 px-2 flex items-center ${isFixed ? '' : 'font-bold'}`}>
                                   <Bars3Icon className={`w-4 h-4 ${isFixed ? "cursor-not-allowed" : "cursor-move"} text-gray-400 mr-2`} />
                                   {attendee.full_name}
+                                  {attendee.is_gratis && !attendee.is_artist ? <BiLogoSketch title="Free Ticket" className='pl-1 w-6 h-6' /> : null }
+                                  {attendee.is_artist ? <RiVipFill title="Artist Ticket" className='pl-1 w-6 h-6' /> : null }
+                                  {attendee.dietary_requirements.selected && attendee.dietary_requirements.selected?.length > 0 ? 
+                                    <div className="ml-1 flex items-center justify-center cursor-pointer relative group">
+                                    <MdRestaurantMenu title="Dietary requirements" className='pl-1 w-6 h-6' />
+                                    <div className="absolute hidden group-hover:block bg-white text-gray-900 text-xs rounded shadow-lg p-2 mt-2 w-48">
+                                      {attendee.dietary_requirements.selected.join(",")}
+                                    </div>
+                                  </div> : null }
                                 </td>
                                 <td className={`py-1 px-2 ${isFixed ? '' : 'font-bold'}`}>
                                   <Link href={`https://www.merseysidelatinfestival.co.uk/preferences?email=${attendee.email.replace("@","%40")}&ticket_number=${attendee.ticket_number}`}>{attendee.group || "No group"}</Link>
