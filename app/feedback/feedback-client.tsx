bb40a017471c62cfac06522142497438e6a1e9df
Adam Bardsley <adam.bardsley@gmail.com>
2024-11-27 09:45:57 +0000

    Adding a feedback form

---
+++ app/feedback/feedback-client.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { FilloutStandardEmbed  } from "@fillout/react";
+import "@fillout/react/style.css";
+
+const FeedbackClient = () => {
+  return <div className="w-full h-[100vh] min-height-32">
+    <FilloutStandardEmbed  filloutId="tMa3MB5yNpus" inheritParameters />;
+    </div>
+}
+
+export default FeedbackClient
