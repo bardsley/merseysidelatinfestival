6aec3d2166b79f9e53fde6e91d8e3d7300d0b0fc
Connor Monaghan <connor1monaghan@gmail.com>
2025-01-03 15:01:49 +0000

    build fixes

--- components/nav/logo.tsx
+++ components/nav/logo.tsx
@@ -1,7 +1,7 @@
 'use client'
-import MlfLogo from '@public/mlf-2.svg';
+// import MlfLogo from '@public/mlf-2.svg';
 
 export default function Logo({className}) {
   // return <MlfLogo className={className} />;
-  return <img src="/newcastle.png" className="h-32"></img>
+  return <img src="/newcastle.png" className={`h-32 ${className}`}></img>
 }
