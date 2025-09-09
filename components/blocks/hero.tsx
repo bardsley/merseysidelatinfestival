c6324de1f9c7d0af9acde6e82d5f965b5cb66f75
Adam Bardsley <adam.bardsley@gmail.com>
2025-09-09 10:01:12 +0100

    Removing border from hero image

--- components/blocks/hero.tsx
+++ components/blocks/hero.tsx
@@ -38,7 +38,7 @@ export const Hero = ({ data }: { data: PageBlocksHero }) => {
             className="relative flex-shrink-0 md:w-2/5 flex justify-center order-first md:hidden md:order-last"
           >
             <Image
-              className={`w-full border h-auto max-w-full rounded-lg mb-3 ${/.*\.svg/.test(data.image.src) ? "p-12 sm:p-24 md:p-0" : "nothing"}`}
+              className={`w-full h-auto max-w-full rounded-lg mb-3 ${/.*\.svg/.test(data.image.src) ? "p-12 sm:p-24 md:p-0" : "nothing"}`}
               style={/.*\.svg/.test(data.image.src) ? { objectFit: "contain" } : { }}
               alt={data.image.alt}
               src={data.image.src}
