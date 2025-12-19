e53e3e1b4ba347a4856c090cd6a7711dbe08f8d1
Adam Bardsley <adam.bardsley@gmail.com>
2025-12-19 01:52:12 +0000

    :art: Updates logo to new SVG format
    Replaces the PNG logo with a more scalable SVG format in the navigation component for improved image quality and performance.
    
    Adds the new SVG file to the project assets.

--- components/nav/logo.tsx
+++ components/nav/logo.tsx
@@ -1,5 +1,5 @@
 'use client'
-import MlfLogo from '@public/delogo.png';
+import MlfLogo from '@public/DE-square.svg';
 
 export default function Logo({className}) {
   return <MlfLogo className={className} />;
