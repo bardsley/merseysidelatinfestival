1b1737dad1f87d5a831cab8c3183a5a089976400
Adam Bardsley <adam.bardsley@gmail.com>
2026-08-09 15:12:37 +0100

    Ignore build artefacts

--- app/artists/client-page.tsx
+++ app/artists/client-page.tsx
@@ -36,62 +36,11 @@ export default function ArtistClientPage(props: ClientPostProps) {
     <div className="text-white grid grid-cols-2 md:grid-cols-3 gap-4 lg:grid-cols-4 max-w-5xl mx-auto my-6 w-full ">
       <h1 className="col-span-2 md:col-span-3 lg:col-span-4 text-5xl font-bold">Artists</h1>
       {data?.artistConnection.edges.filter(
-          (artist) => /^(?!2024).+/.test(artist.node._sys.relativePath)
+          (artist) => /^(?=2026).+/.test(artist.node._sys.relativePath)
         ).map((artistData,index) => {
         const artist = artistData.node;
-        // const date = new Date(post.date);
-        // let formattedDate = "";
-        // if (!isNaN(date.getTime())) {
-        //   formattedDate = format(date, "MMM dd, yyyy");
-        // }
-        // return (
-        //   <Link
-        //     key={post.id}
-        //     href={`/posts/` + post._sys.breadcrumbs.join("/")}
-        //     className="group block px-6 sm:px-8 md:px-10 py-10 mb-8 last:mb-0 bg-gray-50 bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-1000 rounded-md shadow-sm transition-all duration-150 ease-out hover:shadow-md hover:to-gray-50 dark:hover:to-gray-800"
-        //   >
-        //     <h3
-        //       className={`text-gray-700 dark:text-white text-3xl lg:text-4xl font-semibold title-font mb-5 transition-all duration-150 ease-out ${
-        //         titleColorClasses[theme.color]
-        //       }`}
-        //     >
-        //       {post.title}{" "}
-        //       <span className="inline-block opacity-0 group-hover:opacity-100 transition-all duration-300 ease-out">
-        //         <BsArrowRight className="inline-block h-8 -mt-1 ml-1 w-auto opacity-70" />
-        //       </span>
-        //     </h3>
-        //     <div className="prose dark:prose-dark w-full max-w-none mb-5 opacity-70">
-        //       <TinaMarkdown content={post.excerpt} />
-        //     </div>
-        //     <div className="flex items-center">
-        //       <div className="flex-shrink-0 mr-2">
-        //         <Image
-        //           width={500}
-        //           height={500}
-        //           className="h-10 w-10 object-cover rounded-full shadow-sm"
-        //           src={post?.author?.avatar}
-        //           alt={post?.author?.name}
-        //         />
-        //       </div>
-        //       <p className="text-base font-medium text-gray-600 group-hover:text-gray-800 dark:text-gray-200 dark:group-hover:text-white">
-        //         {post?.author?.name}
-        //       </p>
-        //       {formattedDate !== "" && (
-        //         <>
-        //           <span className="font-bold text-gray-200 dark:text-gray-500 mx-2">
-        //             —
-        //           </span>
-        //           <p className="text-base text-gray-400 group-hover:text-gray-500 dark:text-gray-300 dark:group-hover:text-gray-150">
-        //             {formattedDate}
-        //           </p>
-        //         </>
-        //       )}
-        //     </div>
-        //   </Link>
-        // );
         return (
-          <>
-            {/* <div className="">{JSON.stringify(artist._sys,null,2)}</div> */}
+          
             <Link
             key={`${artist.id}-${index}`}
             href={`/artists/` + artist._sys.breadcrumbs.join("/")}
@@ -123,7 +72,7 @@ export default function ArtistClientPage(props: ClientPostProps) {
               </text>
             </svg>
           </Link>
-          </>
+        
           
         )
       })}
